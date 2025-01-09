ceba05b01a833116a79d937482e96815883776b3
miriam590 <miriam.yego@student.moringaschool.com>
2025-01-09 16:16:21 +0300

    index.js

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
+
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  // Create a set to store the numbers we've seen so far
+  const seenNumbers = new Set();
+
+  // Iterate over the array
+  for (const number of array) {
+    // Calculate the complement that would sum with the current number to reach the target
+    const complement = target - number;
+
+    // Check if the complement has already been seen
+    if (seenNumbers.has(complement)) {
+      // If yes, we found a pair, so return true
+      return true;
+    }
+
+    // Add the current number to the set of seen numbers
+    seenNumbers.add(number);
+  }
+
+  // If we've iterated over the entire array and haven't found a pair, return false
+  return false;
 }
 
-/* 
-  Write the Big O time complexity of your function here
-*/
-
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
-
-// You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
+  // Add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
 
@@ -29,6 +36,29 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([1, 2, 3, 4, 5], 9));
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([0, -1, 2, -3, 1], -2));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([], 5));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([5, 5, 5, 5], 10));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([10, 15, 3, 7], 17));
 }
 
 module.exports = hasTargetSum;
